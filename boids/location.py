2391282bdc6c61da4497dc4b78bc403720174f08
Zane Rankin <zrankin@ihme.washington.edu>
2018-11-23 17:44:28 -0600

    bounce off walls

--- boids/location.py
+++ boids/location.py
@@ -21,14 +21,18 @@ class Location:
         # Start clustered in the center with small random velocities
         new_population = pd.DataFrame({
             'x': self.width * (0.4 + 0.2 * np.random.random(count)),
-            'y': self.height * (0.4 + 0.2 * np.random.random(count)),
-            'vx': -0.5 + np.random.random(count),
-            'vy': -0.5 + np.random.random(count),
-        }, index= pop_data.index)
+            'y': self.width * (0.4 + 0.2 * np.random.random(count)),
+            'vx': 10 * np.random.randn(count),  # -0.5 + np.random.random(count),
+            'vy': 10 * np.random.randn(count),  # -0.5 + np.random.random(count),
+        }, index=pop_data.index)
         self.population_view.update(new_population)
 
     def on_time_step(self, event):
         pop = self.population_view.get(event.index)
-        pop.x += pop.vx
-        pop.y += pop.vy
+
+        pop['x'] = pop.apply(lambda row: self.move_boid(row.x, row.vx, self.width), axis=1)
+        pop['vx'] = pop.apply(lambda row: self.avoid_wall(row.x, row.vx, self.width), axis=1)
+        pop['y'] = pop.apply(lambda row: self.move_boid(row.y, row.vy, self.height), axis=1)
+        pop['vy'] = pop.apply(lambda row: self.avoid_wall(row.y, row.vy, self.height), axis=1)
+
         self.population_view.update(pop)
